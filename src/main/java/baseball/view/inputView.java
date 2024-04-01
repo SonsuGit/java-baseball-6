a409d16bc8aa5a20b77e177cf2bfb32113f6a641
Suha Son <dhkd98@gmail.com>
2024-04-01 20:25:29 +0900

    feat: add view

---
+++ src/main/java/baseball/view/inputView.java
@@ -0,0 +1,12 @@
+package baseball.view;
+
+public class inputView {
+
+    public static void requestInput() {
+        System.out.println("숫자를 입력해주세요 : ");
+    }
+
+    public static void retryMessage() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+}
---
+++ src/main/java/baseball/view/outputView.java
@@ -0,0 +1,15 @@
+package baseball.view;
+
+public class outputView {
+
+    public static void printBall(int num) {
+        System.out.println(num + "볼 ");
+    }
+
+    public static void printStrike(int num) {
+        System.out.println(num + "스트라이크 ");
+    }
+    public static void printNothing() {
+        System.out.println("낫싱");
+    }
+}
