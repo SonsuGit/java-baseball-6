1a7473eaab08ff9f4508eeebea01e92a94724625
Suha Son <dhkd98@gmail.com>
2024-04-02 17:32:49 +0900

    test: add ParserTest
    made test code for the first time

---
+++ src/test/java/baseball/utils/ParserTest.java
@@ -0,0 +1,44 @@
+package baseball.utils;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParserTest {
+
+    private Parser parser;
+    private ArrayList<Integer> inputs;
+
+    @BeforeEach
+    void beforeEach(){
+        parser = new Parser();
+        inputs = new ArrayList<>();
+    }
+
+    @AfterEach
+    void afterEach(){
+        inputs.clear();
+    }
+    @DisplayName("Parser 테스트")
+    @Test
+    public void testParseInput() {
+        inputs.add(1);
+        inputs.add(5);
+        inputs.add(2);
+        assertDoesNotThrow(() -> parser.parseInput(inputs, 3, 1, 9));
+    }
+
+    @DisplayName("Parser 에러 테스트")
+    @Test
+    public void testParseInputException() {
+        inputs.add(1);
+        inputs.add(5);
+        //inputs.add(2);
+        assertThrows(IllegalArgumentException.class, () -> parser.parseInput(inputs, 3, 1, 9));
+    }
+}
