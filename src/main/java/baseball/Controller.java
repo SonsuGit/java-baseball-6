7cdc364275778a6494e0db0b62f2d80508a390dd
Suha Son <dhkd98@gmail.com>
2024-04-04 21:33:33 +0900

    feat: updated Controller, added GameService.numberChecker
    계획대로라면 numberChecker을 완성하면 끝...인데

--- src/main/java/baseball/Controller.java
+++ src/main/java/baseball/Controller.java
@@ -1,24 +1,51 @@
 package baseball;
 
+import baseball.model.GameNumber;
+import baseball.model.UserNumber;
+import baseball.service.GameService;
+import baseball.utils.Parser;
+import baseball.utils.Randomizer;
 import baseball.view.inputView;
 import baseball.view.outputView;
+
+import java.util.ArrayList;
 
 public class Controller {
 
     public void run(){
+        setGame();
         startGame();
         askRetry();
     }
-    private void startGame() {
+    private GameNumber gameNumber;
+    private UserNumber userNumber;
+    private Parser parser;
+    private GameService gameService;
+    private int ballCount;
+    private int strikeCount;
 
+    private void setGame() {
         // 기본적인 변수 설정 - ballCount를 0으로 하는 등
+        ballCount = 0;
+        strikeCount = 0;
+        ArrayList<Integer> computerNumber = Randomizer.getRandomNumArraylist(3,1,9);
+        gameNumber = new GameNumber(ballCount, strikeCount, computerNumber);
+        userNumber = new UserNumber();
+    }
 
+    private void startGame() {
         // 입력하라는 메시지 출력
         inputView.printInputRequiredMessage();
 
         // 입력을 받음
+        parser = new Parser();
+        String userInputString = inputView.getUserInput();
+        userNumber.setUserNumbers(parser.stringIntoArrayList(userInputString));
 
-        // 받은 입력이 3스트라이크가 아니면 계속 루프
+        //TODO 받은 입력이 3스트라이크가 아니면 계속 루프
+        if(!gameService.numberChecker(
+                gameNumber.getComputerNumbers(),userNumber.getUserNumbers(),ballCount,strikeCount)
+        ) startGame();
 
         // 맞으면 끝났다는 메시지 출력
         outputView.printGameEnd();
@@ -27,6 +54,8 @@ public class Controller {
 
     private void askRetry() {
         inputView.printRetryMessage();
-        // 그 다음 인풋을 받고, 1이면 run(), 2면 종료
+
+        //TODO 그 다음 인풋을 받고, 1이면 run(), 2면 종료
+
     }
 }
---
+++ src/main/java/baseball/service/GameService.java
@@ -0,0 +1,15 @@
+package baseball.service;
+
+import java.util.ArrayList;
+
+public class GameService {
+    // Todo 컴퓨터 인풋과 유저 인풋을 비교하여 동일하면(=게임 끝) true 를 반환하는 메소드
+    public boolean numberChecker(
+            ArrayList<Integer> computerNumbers, ArrayList<Integer> userNumbers,
+            int ballCount, int strikeCount) {
+        // Todo 루프를 돌아서 체크(strike인지 체크하고 아니면 그때 ball 체크)
+
+        return true;
+    }
+}
+
