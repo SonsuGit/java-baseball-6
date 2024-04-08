e4e857bc5a741debac2e32b4415cd160db070132
Suha Son <dhkd98@gmail.com>
2024-04-08 15:36:25 +0900

    feat: add GameService

--- src/main/java/baseball/service/GameService.java
+++ src/main/java/baseball/service/GameService.java
@@ -1,5 +1,7 @@
 package baseball.service;
 
+import baseball.view.outputView;
+
 import java.util.ArrayList;
 
 public class GameService {
@@ -8,8 +10,43 @@ public class GameService {
             ArrayList<Integer> computerNumbers, ArrayList<Integer> userNumbers,
             int ballCount, int strikeCount) {
         // Todo 루프를 돌아서 체크(strike인지 체크하고 아니면 그때 ball 체크)
+        for(int index = 0; index<userNumbers.size(); index++){
+            if(strikeCounter(userNumbers.get(index), index, computerNumbers)){
+                strikeCount += 1;
+            }else if(ballCounter(userNumbers.get(index), computerNumbers)){
+                ballCount += 1;
+            }
+        }
+        if(ballCount==0 && strikeCount == 0){
+            outputView.printNothing();
+        }
+        if(ballCount!=0){
+            outputView.printBall(ballCount);
+        }
+        if(strikeCount!=0){
+            outputView.printStrike(strikeCount);
+        }
+        System.out.println();
 
-        return true;
+        if(strikeCount == 3){
+            return true;
+        }
+        return false;
+    }
+    private boolean ballCounter(int userNumber, ArrayList<Integer> computerNumbers){
+        for(int computerNumber : computerNumbers){
+            if (userNumber == computerNumber){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean strikeCounter(int userNum, int index, ArrayList<Integer> computerNumbers) {
+        if(userNum == computerNumbers.get(index)){
+            return true;
+        }
+        return false;
     }
 }
 
