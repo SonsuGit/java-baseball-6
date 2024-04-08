a760b82dfdfa48355fca170f7b6a7d608dfcec72
Suha Son <dhkd98@gmail.com>
2024-04-08 15:08:05 +0900

    fix: change println to print due to code
    because of the way i wrote(call method several times to print things) changed println to print

--- src/main/java/baseball/view/outputView.java
+++ src/main/java/baseball/view/outputView.java
@@ -7,14 +7,14 @@ public class outputView {
     }
 
     public static void printBall(int num) {
-        System.out.println(num + "볼 ");
+        System.out.print(num + "볼 ");
     }
 
     public static void printStrike(int num) {
-        System.out.println(num + "스트라이크 ");
+        System.out.print(num + "스트라이크 ");
     }
     public static void printNothing() {
-        System.out.println("낫싱");
+        System.out.print("낫싱");
     }
     public static void printGameEnd() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
